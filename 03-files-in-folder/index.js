720dbcc186bc57bca7ffd895c28c108446f463a5
salavat <asfanov@gmail.com>
2022-11-05 12:50:09 +0400

    fix: replacing paths in task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,15 +1,14 @@
 const path = require('path');
 const fs = require('fs');
  
-const pathDir = path.join(__dirname, 'secret-folder'); //путь на директорию папки secret-folder
+const pathDir = path.join(__dirname, 'secret-folder'); 
 // console.log(pathDir);
 
 fs.readdir(pathDir,{withFileTypes: true}, (err, files) => {
-    if (err)
-      console.log(err);
+    if (err) console.log(err);
     else {
       files.forEach(file => {
-        fs.stat(`./03-files-in-folder/secret-folder/${file.name}`, (err, stats) => {
+        fs.stat(`${pathDir}/${file.name}`, (err, stats) => {
             if(err) console.log(err);
             else {
                 if(stats.isFile() != true){
@@ -17,13 +16,10 @@ fs.readdir(pathDir,{withFileTypes: true}, (err, files) => {
                 } else {
                     console.log(file.name.replace(/.\w*$/i,'') + ' - ' +  file.name.replace(/^\w*./i,'') + ' - ' + stats.size/1000 + 'kb');
                 }
-                
-                    
             }
-        });        
+        });
       })
     }
-  
 })
 
 
